96d062dddb5eb743ed06a91ee593d55e99ae8af2
Luis Rodriguez <luisrodge@outlook.com>
2022-12-15 19:44:48 -0600

    tw changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline text-white">
+          Hello world!
+        </h1>
       </main>
     </>
   );
