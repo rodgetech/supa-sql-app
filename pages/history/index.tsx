7cf8462772012d158e337ded97e75e55110994b6
Luis Rodriguez <luisrodge@outlook.com>
2022-12-18 13:07:51 -0600

    handle history delete

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -1,9 +1,11 @@
 import {
   createServerSupabaseClient,
+  createBrowserSupabaseClient,
   User,
 } from "@supabase/auth-helpers-nextjs";
 import { GetServerSidePropsContext } from "next";
 import Link from "next/link";
+import Router, { useRouter } from "next/router";
 import Code from "../../components/Code";
 import { getRelativeTime } from "../../utils/helpers";
 
@@ -44,25 +46,71 @@ export default function HistoryList({
   user: User;
   history: any[];
 }) {
+  const router = useRouter();
+  const supabase = createBrowserSupabaseClient();
+  const remove = async (id?: string) => {
+    try {
+      let delErr: any;
+      if (id) {
+        const { error } = await supabase
+          .from("translation_histories")
+          .delete()
+          .eq("id", id);
+        delErr = error;
+      } else {
+        const { error } = await supabase
+          .from("translation_histories")
+          .delete()
+          .in(
+            "id",
+            history.map((h) => h.id)
+          );
+        delErr = error;
+      }
+      if (!delErr) router.push("/history");
+    } catch (err) {
+      console.log("Delete failed", err);
+    }
+  };
+
   return (
     <div className="mx-auto mt-6 w-8/12 pb-20">
-      <div className="mt-12">
+      <div className="mt-12 flex items-end justify-between">
         <h2 className="text-xl text-white">Your history</h2>
+        {history && history.length > 0 && (
+          <p
+            className="z-50 cursor-pointer text-zinc-500 hover:text-zinc-300"
+            onClick={() => remove()}
+          >
+            Clear all
+          </p>
+        )}
       </div>
       <div className="mt-8">
         <div className="grid grid-cols-2 gap-x-4 gap-y-5">
           {history.map((h, i) => (
-            <Link href={`/history/${h.id}`} key={i}>
-              <div className="rounded-md border border-zinc-800 bg-zinc-800/20 p-4 hover:border-zinc-700">
+            <div
+              className="rounded-md border border-zinc-800 bg-zinc-800/20 p-4 hover:border-zinc-700"
+              key={i}
+            >
+              <Link href={`/history/${h.id}`}>
                 <p className="mb-3 text-white">{h.query_instructions}</p>
                 <div className="max-h-[150px] overflow-hidden">
                   <Code sql={h.query_completion} enableClipboard={false} />
                 </div>
-                <p className="mt-4 text-zinc-500">
+              </Link>
+              <div className="mt-4 flex justify-between">
+                <p className="  text-zinc-500">
                   {getRelativeTime(new Date(), new Date(h.created_at))}
                 </p>
+                <p
+                  className="z-50 cursor-pointer text-zinc-500 hover:text-zinc-300"
+                  onClick={() => remove(h.id)}
+                >
+                  Delete
+                </p>
               </div>
-            </Link>
+            </div>
           ))}
         </div>
       </div>
